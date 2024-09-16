bffccbfeb50eaa8db45b371edacf01f8b4d7c55f
LAPTOP-1QRLTJDB\DUC TIN <tin.hoduc1608@hcmut.edu.vn>
2024-09-16 11:23:27 +0700

    Init exercise 8 lab 1

---
+++ sourceCode/Core/Inc/ex8.h
@@ -0,0 +1,16 @@
+/*
+ * ex8.h
+ *
+ *  Created on: Sep 16, 2024
+ *      Author: DUC TIN
+ */
+
+#ifndef INC_EX8_H_
+#define INC_EX8_H_
+
+#include "main.h"
+
+void init_ex8();
+void setNumberOnClock(int num);
+
+#endif /* INC_EX8_H_ */
